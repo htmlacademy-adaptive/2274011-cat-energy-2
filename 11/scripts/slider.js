baed13739f372f3fe4f5b3d6c3d68a8b934d3202
keksobot <keksobot@users.noreply.github.com>
2025-12-27 15:26:46 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2274011-cat-energy-2@744caac8c07331676c528ad7b921999041835cd1 🚀

--- 11/scripts/slider.js
+++
@@ -1,1 +0,0 @@
-var r=document.querySelector(".example__slider"),e=document.querySelector(".slider__range-js");e.addEventListener("input",()=>{r.style.setProperty("--value",`${e.value}%`)});
--- scripts/burger.js
+++ scripts/burger.js
@@ -1,1 +1,1 @@
-var e=document.querySelector(".nav"),n=e.querySelector(".nav__toggle");e.classList.remove("nav--js");n.addEventListener("click",()=>{e.classList.toggle("nav--nojs")});
+var e=document.querySelector(".nav"),n=e.querySelector(".nav__toggle");e.classList.remove("nav--nojs");e.classList.add("nav--js");n.addEventListener("click",()=>{e.classList.toggle("nav--opened")});
